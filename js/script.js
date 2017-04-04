f775cd408e51d1f788f61a0c04436f34cebb07d8
Gagan Ganapathy <gaganganapathyas@yahoo.in>
2017-04-04 22:20:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 		if(val == '') {
 			$("#query").css('box-shadow', 'inset 0 1px 2px rgba(0,0,0,0.1), 0 0 0 6px #dd2826');
 			// $("#results").html('');
+			$("#s_history").html('');
 		}
 		else {
 			$("#query").css('box-shadow', 'inset 0 1px 2px rgba(0,0,0,0.1), 0 0 0 6px #f0f0f0');
